511e31c756ce77ba948fd527af1c1c2593616766
Damian Holona <damianholona074@gmail.com>
2025-11-24 15:24:16 +0100

    + user interface class and enum for menu options

---
+++ shop_project/src/ui/MenuOption.java
@@ -0,0 +1,32 @@
+package ui;
+
+import util.Constants;
+
+enum MenuOption {
+    SHOW_PRODUCTS(1, "Przeglądaj produkty"),
+    ADD_TO_CART(2, "Dodaj do koszyka"),
+    REMOVE_FROM_CART(3, "Usuń z koszyka"),
+    PLACE_AN_ORDER(4, "Złóż zamówienie"),
+    EXIT(5, "Wyjdź");
+
+    private final int value;
+    private final String description;
+
+    MenuOption(int value, String description) {
+        this.value = value;
+        this.description = description;
+    }
+
+    public static MenuOption getOptionFromInt(int number) {
+        if (number > MenuOption.values().length || number < 0) {
+            throw new IllegalArgumentException(Constants.INVALID_OPTION_MESS);
+        }
+        return MenuOption.values()[number-1];
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%d. %s", value, description);
+    }
+}
+
---
+++ shop_project/src/ui/UserInterface.java
@@ -0,0 +1,50 @@
+package ui;
+
+import manager.ProductManager;
+import model.Cart;
+import util.Constants;
+
+public class UserInterface {
+    private final ProductManager manager;
+    private final Cart cart = new Cart();
+
+    public UserInterface(ProductManager manager) {
+        this.manager = manager;
+    }
+
+    public void start() {
+        boolean running = true;
+
+        while (running) {
+            DataPrinter.print("Witamy w naszym sklepie");
+
+            printMenu();
+            try {
+                running = chooseOption(running);
+            } catch (IllegalArgumentException e) {
+                DataPrinter.print(e.getMessage());
+            }
+        }
+    }
+
+    private boolean chooseOption(boolean running) {
+        DataPrinter.print("Wybierz co chcesz zrobić");
+        MenuOption option = MenuOption.getOptionFromInt(DataReader.getIntFromUser());
+
+        switch(option) {
+            case SHOW_PRODUCTS -> manager.showProducts();
+            case ADD_TO_CART -> DataPrinter.print("Funkcjonalnosc wkrotce");
+            case REMOVE_FROM_CART -> DataPrinter.print("Funkcjonalnosc wkrotce");
+            case PLACE_AN_ORDER -> DataPrinter.print("Funkcjonalnosc wkrotce");
+            case EXIT -> running = false;
+            default -> throw new IllegalArgumentException(Constants.INVALID_OPTION_MESS);
+        }
+        return running;
+    }
+
+    private void printMenu() {
+        for (MenuOption option : MenuOption.values()) {
+            DataPrinter.print(option.toString());
+        }
+    }
+}
