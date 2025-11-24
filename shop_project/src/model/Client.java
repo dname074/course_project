295a586d66dc6425b4d021a1a6b9f41977d82229
Damian Holona <damianholona074@gmail.com>
2025-11-24 14:42:06 +0100

    + task5, client class

---
+++ shop_project/src/model/Client.java
@@ -0,0 +1,23 @@
+package model;
+
+public class Client {
+    private String firstName;
+    private String lastName;
+    private int age;
+    private String city;
+    private String country;
+
+    public Client(String firstName, String lastName, int age, String city, String country) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.city = city;
+        this.country = country;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s \n%s \n%d \n%s \n%s\n",
+                firstName, lastName, age, city, country);
+    }
+}
