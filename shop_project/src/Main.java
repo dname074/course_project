ae26956851f9a81d7a76a9393e7f4f9695784f09
Damian Holona <damianholona074@gmail.com>
2025-11-24 15:26:26 +0100

    + interface test

--- shop_project/src/Main.java
+++ shop_project/src/Main.java
@@ -1,5 +1,21 @@
+import manager.ProductManager;
+import model.Computer;
+import model.Magazine;
+import model.Smartphone;
+import ui.UserInterface;
+
+import java.math.BigDecimal;
+
 public class Main {
     public static void main(String[] args) {
+        Magazine magazine = new Magazine();
+        ProductManager manager = new ProductManager(magazine);
+        manager.addProductToMagazine(new Computer(1, "Komputer1", BigDecimal.valueOf(3500.0), 50,
+                "16 GB", "Intel Core i5", "1TB", "Geforce GTX 1660", "Windows 11"));
+        manager.addProductToMagazine(new Smartphone(2, "Iphone 15", BigDecimal.valueOf(2900.0), 50,
+                "Black", "3349 mAh", "Tylny 48 Mpx + 12 Mpx, Przedni 12 Mpx",  "iOS"));
 
+        UserInterface app = new UserInterface(manager);
+        app.start();
     }
 }
