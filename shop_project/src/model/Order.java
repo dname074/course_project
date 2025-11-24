08403be4faeb0c3d7e8211900acc2393c03b69b3
Damian Holona <damianholona074@gmail.com>
2025-11-24 19:52:18 +0100

    + order date save and add date formatter to Constants class

--- shop_project/src/model/Order.java
+++ shop_project/src/model/Order.java
@@ -1,22 +1,27 @@
 package model;
 
+import util.Constants;
+
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public class Order {
-    private Client client;
-    private List<Product> cart;
-    private BigDecimal totalPrice;
+    private final Client client;
+    private final List<Product> cart;
+    private final BigDecimal totalPrice;
+    private final LocalDateTime orderDate;
 
     public Order(Client client, List<Product> cart, BigDecimal totalPrice) {
         this.client = client;
         this.cart = cart;
         this.totalPrice = totalPrice;
+        orderDate = LocalDateTime.now();
     }
 
     @Override
     public String toString() {
-        return String.format("Client information:\n%sCart:\n%sTotal price:\n%.2f",
-                client.toString(), cart.toString(), totalPrice);
+        return String.format("Client information:\n%s\nCart:\n%s\nTotal price:\n%.2f\nOrder date: %s",
+                client.toString(), cart.toString(), totalPrice, orderDate.format(Constants.dateFormat));
     }
 }
--- shop_project/src/util/Constants.java
+++ shop_project/src/util/Constants.java
@@ -1,5 +1,8 @@
 package util;
 
+import java.time.format.DateTimeFormatter;
+
 public class Constants {
     public static final String INVALID_OPTION_MESS = "Podano nieistniejącą opcję";
+    public static final DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
 }
