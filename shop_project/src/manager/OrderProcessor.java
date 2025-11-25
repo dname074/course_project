6837bca6f004b3a3bcda4264c69b18187d925cdf
Damian Holona <damianholona074@gmail.com>
2025-11-26 00:17:48 +0100

    + delete order repository class

--- shop_project/src/manager/OrderProcessor.java
+++ shop_project/src/manager/OrderProcessor.java
@@ -3,16 +3,13 @@ package manager;
 import exception.FileWriteException;
 import model.Invoice;
 import model.Order;
-import model.OrderRepository;
 import storage.csv.InvoiceCsvManager;
 
 public class OrderProcessor {
-    private static final OrderRepository orders = new OrderRepository();
     private static final InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
     private static final InvoiceCsvManager csvManager = new InvoiceCsvManager();
 
     public static void takeAnOrder(Order order) throws FileWriteException {
-        orders.addOrder(order);
         Invoice invoice = invoiceGenerator.generateInvoice(order);
         csvManager.saveInvoice(invoice);
     }
