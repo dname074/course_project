0f1f83659f764e9336057e56ec4805cdc4991547
Damian Holona <damianholona074@gmail.com>
2025-12-03 23:18:13 +0100

    + remove unnecessary classes and constants

--- shop_project/src/Main.java
+++ shop_project/src/Main.java
@@ -1,13 +1,12 @@
-import configuration.ConfigurationManager;
-import configuration.ConfigurationValidator;
+import manager.ConfigurationManager;
 import exception.ProductAlreadyInSystemException;
 import manager.CartManager;
 import manager.InvoiceGenerator;
 import manager.OrderProcessor;
 import manager.ProductManager;
 import model.Cart;
-import configuration.Configuration;
-import model.ConfigType;
+import model.Configuration;
+import model.ConfigOption;
 import model.Magazine;
 import model.Product;
 import promotion.Promotion;
@@ -47,8 +46,7 @@ public class Main {
         OrderProcessor orderProcessor = new OrderProcessor(invoiceGenerator, invoiceCsvManager, executorService);
         Cart cart = new Cart();
         CartManager cartManager = new CartManager(cart, productManager, orderProcessor);
-        ConfigurationValidator productConfigValidator = new ConfigurationValidator();
-        ConfigurationManager productConfigManager = new ConfigurationManager(productConfigValidator);
+        ConfigurationManager productConfigManager = new ConfigurationManager();
 
         UserInterface app = new UserInterface(productManager, cartManager, promoManager, productConfigManager);
         app.start();
@@ -57,8 +55,8 @@ public class Main {
 
     private static void generateTestData(ProductManager productManager) {
         try {
-            List<Configuration> wypasionyKomp = List.of(new Configuration(ConfigType.RAM_8), new Configuration(ConfigType.DISK_500),
-                    new Configuration(ConfigType.GPU_1660), new Configuration(ConfigType.PROCESSOR_1), new Configuration(ConfigType.COLOR_BLACK));
+            List<Configuration> wypasionyKomp = List.of(new Configuration(ConfigOption.RAM_8), new Configuration(ConfigOption.DISK_500),
+                    new Configuration(ConfigOption.GPU_1660), new Configuration(ConfigOption.PROCESSOR_1), new Configuration(ConfigOption.COLOR_BLACK));
             productManager.addProductToMagazine(new Product(1, "WypasionyKomp", BigDecimal.valueOf(1500), 50, wypasionyKomp));
         } catch (ProductAlreadyInSystemException e) {
             DataPrinter.print(e.getMessage());
--- shop_project/src/util/Constants.java
+++ shop_project/src/util/Constants.java
@@ -1,7 +1,6 @@
 package util;
 
 import java.time.format.DateTimeFormatter;
-import java.util.List;
 
 public class Constants {
     public static final String INVALID_OPTION_MESS = "Podano nieistniejącą opcję";
@@ -9,9 +8,4 @@ public class Constants {
     public static final String CSV_FILE_PATH = "data/invoices.csv";
     public static final String ORDER_PROCESS_FAILURE = "Błąd przy przetwarzaniu zamówienia";
     public static final int MAX_ORDERS_PROCESSED = 4;
-    public static final List<String> RAM_OPTIONS = List.of("8 GB", "16 GB");
-    public static final List<String> DISK_OPTIONS = List.of("500 GB", "1000 GB");
-    public static final List<String> COMPUTER_OS = List.of("Windows 10", "Windows 11");
-    public static final List<String> SMARTPHONE_OS = List.of("Android 14", "Android 15");
-    public static final List<String> BATTERY_CAPACITY_OPTIONS = List.of("3000 mAh", "5000 mAh");
 }
